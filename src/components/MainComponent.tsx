076afe353bb9b6abd14b22551b33e2047d9b17e8
illyagm <workmail753@gmail.com>
2021-02-01 17:12:35 +0100

    functional basic structure with main component

---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const MainComponent = () => {
+    return (
+        <div>
+            <p>Hola</p>
+        </div>
+    );
+}
+
+export default MainComponent;
